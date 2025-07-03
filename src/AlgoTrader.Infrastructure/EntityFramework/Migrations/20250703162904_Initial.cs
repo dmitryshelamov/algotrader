46d372823eed6392e5b188ecfb63b9e5841bf99d
Dmitry Shelamov <dmitryshelamov@gmail.com>
2025-07-03 23:29:54 +0700

    add ticker migration

---
+++ src/AlgoTrader.Infrastructure/EntityFramework/Migrations/20250703162904_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlgoTrader.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:market_type", "spot,futures");
+
+            migrationBuilder.CreateTable(
+                name: "tickers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ticker_left = table.Column<string>(type: "text", nullable: false),
+                    ticker_right = table.Column<string>(type: "text", nullable: false),
+                    market_type = table.Column<string>(type: "text", nullable: false),
+                    ticker_start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tickers", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tickers");
+        }
+    }
+}
